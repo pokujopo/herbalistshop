495e3427ec47eb0ea01c50468106f6a3a1d5b29d
pokujopo <pokujopo@gmail.com>
2026-04-12 16:41:16 +0000

    Fix import path for img1 in Banner component

--- src/componets/Banner.jsx
+++ src/componets/Banner.jsx
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import img1 from "../src/assets/natural-remedies-1315042436-1024x577.jpg"
+import img1 from "../assets/natural-remedies-1315042436-1024x577.jpg"
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 function Banner() {
